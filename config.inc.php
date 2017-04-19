f5db6cf4e09f3604855f80aa40231539353e68a4
grantypantyyy <grant.grismore@seattlecolleges.edu>
2017-04-18 20:37:11 -0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -11,7 +11,7 @@
 	// indexed from zero upwards.
 
 	// Display name for the server on the login screen
-	$conf['servers'][0]['desc'] = 'Jwade005 PostgreSQL';
+	$conf['servers'][0]['desc'] = 'GRANT PostgreSQL';
 
 	// Hostname or IP address for server.  Use '' for UNIX domain socket.
 	// use 'localhost' for TCP/IP connection on this computer
