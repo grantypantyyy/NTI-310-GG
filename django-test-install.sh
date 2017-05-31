3bf426481feb6ce939f69d08d5e55548c56db3df
grantypantyyy <grant.grismore@seattlecolleges.edu>
2017-05-31 16:53:04 -0700

    Update django-test-install.sh

--- django-test-install.sh
+++ django-test-install.sh
@@ -9,7 +9,7 @@ sudo systemctl enable httpd.service
 echo "Starting Apache Server..."
 sudo systemctl start httpd.service
 
-echo "Cloning jwade005's github..."
+echo "Cloning Grant's github..."
 sudo yum -y install git
 git clone https://github.com/grantypantyyy/NTI-310-GG.git
 
